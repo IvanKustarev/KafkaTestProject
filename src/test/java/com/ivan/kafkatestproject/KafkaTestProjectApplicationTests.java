d4bd31e066952489f494c5b31b2bf897154e3c60
IvanKustarev <ivan.kustarev@gmail.com>
2022-01-30 15:07:18 +0300

    Initial commit

---
+++ src/test/java/com/ivan/kafkatestproject/KafkaTestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivan.kafkatestproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaTestProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
